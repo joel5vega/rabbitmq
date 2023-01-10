59ad6c3c1e4c5a49244a2cc4f92f901748a7173c
joel5vega <joel5vega@gmail.com>
2023-01-09 20:54:07 -0400

    finish lab 1

---
+++ Readme.md
@@ -0,0 +1,2 @@
+##Running Rabbit 3.11
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.11-management
---
+++ new_task.py
@@ -0,0 +1,7 @@
+import sys, pika
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='', routing_key='hello', body=message)
+
+print(" [x] Sent %r" % message)
